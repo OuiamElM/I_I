b5c8e2d17986e39341c88187e9a1b60beae8dfc0
OuiamElM <118132232+OuiamElM@users.noreply.github.com>
2024-01-28 03:31:38 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,34 @@
-
-/* Moves */
 document.addEventListener('DOMContentLoaded', function () {
-   
     var toggleButton = document.getElementById('toggleButton');
     var firstImage = document.getElementById('firstImage');
     var secondImage = document.getElementById('secondImage');
     var thirdImage = document.getElementById('thirdImage');
-  
-    //first image initially
+    var myAudio = document.getElementById('myAudio'); // Added this line
+
+    // First image initially
     firstImage.style.display = 'block';
     secondImage.style.display = 'none';
     thirdImage.style.display = 'none';
-  
-    //click event for the button
+
+    // Click event for the button
     toggleButton.addEventListener('click', function () {
+        // Determine which image is currently displayed and toggle to the next one
+        if (firstImage.style.display === 'block') {
+            firstImage.style.display = 'none';
+            secondImage.style.display = 'block';
+            thirdImage.style.display = 'none';
+        } else if (secondImage.style.display === 'block') {
+            firstImage.style.display = 'none';
+            secondImage.style.display = 'none';
+            thirdImage.style.display = 'block';
+        } else {
+            // If the third image is currently displayed, loop back to the first one
+            firstImage.style.display = 'block';
+            secondImage.style.display = 'none';
+            thirdImage.style.display = 'none';
+        }
 
-      // Determine which image is currently displayed and toggle to the next one
-      if (firstImage.style.display === 'block') {
-        firstImage.style.display = 'none';
-        secondImage.style.display = 'block';
-        thirdImage.style.display = 'none';
-      } else if (secondImage.style.display === 'block') {
-        firstImage.style.display = 'none';
-        secondImage.style.display = 'none';
-        thirdImage.style.display = 'block';
-      } else {
-        
-        // If the third image is currently displayed, loop back to the first one
-        firstImage.style.display = 'block';
-        secondImage.style.display = 'none';
-        thirdImage.style.display = 'none';
-      }
+        // Play the audio when the button is clicked
+        myAudio.play();
     });
-  });
-
+});
